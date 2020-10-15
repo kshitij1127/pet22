d2fa442d5f9bd1923ad8ed37cf9776d72a92b195
kshitij <63649035+kshitij1127@users.noreply.github.com>
2020-10-15 18:07:35 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,114 @@
+//Create variables here
+var dog,happydog,database,foodS,foodStock,dogimg,happydogimg,milkbottle,feed,addFood,food,lastFed,fedTime
+
+function preload()
+{
+  //load images here
+  dogimg = loadImage("images/dogImg1.png")
+  happydogimg = loadImage("images/dogImg.png")
+ 
+}
+
+function setup() {
+  createCanvas(1000, 400)
+  database = firebase.database();
+  food = new Food()
+  foodStock = database.ref('Food')
+  foodStock.on("value",readStock)
+
+  dog = createSprite(200,200)
+  dog.addImage(dogimg)
+  dog.scale = 0.3
+ 
+ 
+
+  feed = createButton("Feed The Dog")
+feed.position(700,95)
+feed.mousePressed(feedDog)
+
+addFood = createButton("Add Food")
+addFood.position(800,95)
+addFood.mousePressed(addFood)
+
+
+
+  
+}
+
+
+function draw() {  
+background(46,139,87)
+
+
+food.display()
+
+fedTime = database.ref('FeedTime')
+fedTime.on("value",function(data){
+  lastFed = data.val();
+})
+
+if(lastFed >= 12){
+  text("last feed : " + lastFed%12 + "PM",350,30)
+}else if(lastFed === 0){
+  text("last feed : 12AM",350,30)
+}else{
+  text("last feed : " + lastFed + "AM",350,30)
+}
+
+stroke(0,100,255)
+strokeWeight(5)
+text("food stock : " + foodS,50,50)
+textSize(20)
+stroke(255,0,0)
+strokeWeight(7)
+text("press the up arrow to feed the hungry dog !",10,300)
+  drawSprites();
+  //add styles here
+
+}
+
+function readStock(data){
+foodS = data.val();
+food.updateFoodStock(foodS)
+}
+
+function writeStock(x){
+  if(x <= 0){
+  x = 0
+  
+  }else{
+    x = x-1
+  }
+database.ref('/').update({
+  Food : x
+})
+
+stroke(255,255,254)
+strokeWeight(5)
+textSize(15)
+
+
+}
+
+
+function feedDog(){
+dog.addImage(happydog)
+food.updateFoodStock(food.getFoodStock()-1)
+database.ref('/').update({
+  Food : food.getFoodStock()-1,
+  FeedTime : hour()
+})
+}
+
+
+function addFood(){
+  foodS++
+  database.ref('/').update({
+    Food : foodS
+  })
+
+  
+}
+
+
+
